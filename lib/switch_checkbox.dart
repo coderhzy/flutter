d86cacc32f744eaee0b5c576f98a8b9a5e00ead5
hzy <1257664828@qq.com>
2021-04-24 11:29:04 +0800

    单选开关&复选框

--- lib/switch_checkbox.dart
+++ lib/switch_checkbox.dart
@@ -15,27 +15,32 @@ class _SwitchAndCheckBoxTestRouteState
   bool _checkboxSelected = true; //维护复选框状态
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Switch(
-          value: _switchSelected, //当前状态
-          onChanged: (value) {
-            //重新构建页面
-            setState(() {
-              _switchSelected = value;
-            });
-          },
-        ),
-        Checkbox(
-          value: _checkboxSelected,
-          activeColor: Colors.red, //选中时的颜色
-          onChanged: (value) {
-            setState(() {
-              _checkboxSelected = value;
-            });
-          },
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Switch&CheckBox'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Switch(
+            value: _switchSelected, //当前状态
+            onChanged: (value) {
+              //重新构建页面
+              setState(() {
+                _switchSelected = value;
+              });
+            },
+          ),
+          Checkbox(
+            value: _checkboxSelected,
+            activeColor: Colors.red, //选中时的颜色
+            onChanged: (value) {
+              setState(() {
+                _checkboxSelected = value;
+              });
+            },
+          )
+        ],
+      ),
     );
   }
 }
